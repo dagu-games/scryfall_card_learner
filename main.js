e089b2e81065bc2acec89d0dd9e6cc0301cc5105
Doug Kihlken <dkihlkenproductions@gmail.com>
2021-05-25 17:44:36 -0500

    new scryfall logic to be less taxing on their api

--- main.js
+++ main.js
@@ -63,6 +63,8 @@ var cards = [
 
 
 
+
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -110,6 +112,124 @@ var app = new Vue({
   methods: {
     clearData: function() {
       localStorage.save_data = "";
+    },
+    scryfall_get_func: function() {
+      const Http = new XMLHttpRequest();
+      var query = "";
+      if (this.seed_string = "") {
+        query = "lang:english";
+      } else {
+        query = this.seed_string + " lang:english";
+      }
+      const url = 'https://api.scryfall.com/cards/search?order=' + this.seed_sort + '&unique=' + this.seed_unique + '&dir=' + this.seed_direction + '&include_extras=false&page=' + this.scryfall_page + '&q=' + encodeURI(query);
+      Http.open("GET", url);
+
+      Http.onreadystatechange = (e) => {
+
+
+        if (Http.readyState == 4 && Http.status == 200) {
+          //console.log(Http.responseText);
+          var response = JSON.parse(Http.responseText);
+
+
+
+
+          for (var i = 0; i < response.data.length; i++) {
+            var found = false;
+            var tcard = {};
+            for (var j = 0; j < this.cards.length; j++) {
+              if (this.cards[j].id == response.data[i].id) {
+                found = true;
+              }
+            }
+            if (found != true) {
+
+
+              if (response.data[i].card_faces == null) {
+                //a single faced card
+                tcard.name = response.data[i].name;
+                tcard.id = response.data[i].id;
+                tcard.mana_cost = response.data[i].mana_cost;
+                tcard.type = response.data[i].type_line;
+                tcard.oracle_text = response.data[i].oracle_text.replaceAll(tcard.name, "[CARD NAME]");
+                if (response.data[i].power != null) {
+                  tcard.oracle_text += "\n" + response.data[i].power + "/" + response.data[i].toughness;
+                }
+                tcard.card_links = [];
+                tcard.art_links = [];
+                if (response.data[i].image_uris != null) {
+                  if (response.data[i].image_uris.png != null) {
+                    tcard.card_links.push(response.data[i].image_uris.png);
+                  }
+                  if (response.data[i].image_uris.art_crop != null) {
+                    tcard.art_links.push(response.data[i].image_uris.art_crop);
+                  }
+                }
+              } else {
+                //a double (or more) faced card
+                tcard.id = response.data[i].id;
+                tcard.name = response.data[i].card_faces[0].name;
+                tcard.mana_cost = response.data[i].card_faces[0].mana_cost;
+                tcard.type = response.data[i].card_faces[0].type_line;
+                tcard.oracle_text = response.data[i].card_faces[0].oracle_text.replaceAll(response.data[i].card_faces[0].name, "[CARD NAME]");
+                if (response.data[i].card_faces[0].power != null) {
+                  tcard.oracle_text += "\n" + response.data[i].card_faces[0].power + "/" + response.data[i].card_faces[0].toughness;
+                }
+                tcard.card_links = [];
+                tcard.art_links = [];
+                if (response.data[i].card_faces[0].image_uris != null) {
+                  if (response.data[i].card_faces[0].image_uris.png != null) {
+                    tcard.card_links.push(response.data[i].card_faces[0].image_uris.png);
+                  }
+                  if (response.data[i].card_faces[0].image_uris.art_crop != null) {
+                    tcard.art_links.push(response.data[i].card_faces[0].image_uris.art_crop);
+                  }
+                }
+                for (var j = 1; j < response.data[i].card_faces.length; j++) {
+                  tcard.name += "  //  " + response.data[i].card_faces[j].name;
+                  tcard.mana_cost += "  //  " + response.data[i].card_faces[j].mana_cost;
+                  tcard.type += "  //  " + response.data[i].card_faces[j].type_line;
+                  tcard.oracle_text += "  //  " + response.data[i].card_faces[j].oracle_text.replaceAll(response.data[i].card_faces[j].name, "[CARD NAME]");
+                  if (response.data[i].card_faces[j].power != null) {
+                    tcard.oracle_text += "\n" + response.data[i].card_faces[j].power + "/" + response.data[i].card_faces[j].toughness;
+                  }
+                  tcard.card_links = [];
+                  tcard.art_links = [];
+                  if (response.data[i].card_faces[j].image_uris != null) {
+                    if (response.data[i].card_faces[j].image_uris.png != null) {
+                      tcard.card_links.push(response.data[i].card_faces[j].image_uris.png);
+                    }
+                    if (response.data[i].card_faces[j].image_uris.art_crop != null) {
+                      tcard.art_links.push(response.data[i].card_faces[j].image_uris.art_crop);
+                    }
+                  }
+                }
+              }
+              this.cards.push(tcard);
+              this.question_order.push(this.cards.length - 1);
+              this.add_count--;
+
+
+            }
+            if(this.add_count <=0){
+              break;
+            }
+          }
+
+
+          if (this.add_count > 0) {
+            this.scryfall_page++;
+            setTimeout(function(){
+              app.scryfall_get_func();
+            }, 100);
+          }else{
+            this.refreshQuestions();
+            this.saveData();
+          }
+        }
+      };
+
+      Http.send();
     },
     showResults: function() {
       return this.show_results;
@@ -159,7 +279,6 @@ var app = new Vue({
         cards: this.cards,
         question_order: this.question_order,
         round: this.round,
-        scryfall_page: this.scryfall_page,
         seed_string: this.seed_string,
         seed_count: this.seed_count,
         seed_sort: this.seed_sort,
@@ -248,7 +367,6 @@ var app = new Vue({
         this.cards = save_data.cards;
         this.question_order = save_data.question_order;
         this.round = save_data.round;
-        this.scryfall_page = save_data.scryfall_page;
         this.seed_string = save_data.seed_string;
         this.seed_count = save_data.seed_count;
         this.seed_sort = save_data.seed_sort;
@@ -389,212 +507,20 @@ var app = new Vue({
       }
     },
     add_card: function() {
-      const Http = new XMLHttpRequest();
-      var query = "";
-      if (this.seed_string = "") {
-        query = "lang:english";
-      } else {
-        query = this.seed_string + " lang:english";
-      }
-      const url = 'https://api.scryfall.com/cards/search?order=' + this.seed_sort + '&unique=' + this.seed_unique + '&dir=' + this.seed_direction + '&include_extras=false&page=' + this.scryfall_page + '&q=' + encodeURI(query);
-      Http.open("GET", url);
-
-      Http.onreadystatechange = (e) => {
-
-
-        if (Http.readyState == 4 && Http.status == 200) {
-          //console.log(Http.responseText);
-          var response = JSON.parse(Http.responseText);
-
-
-          var tcard = {};
-
-          for (var i = 0; i < response.data.length; i++) {
-            var found = false;
-            for (var j = 0; j < this.cards.length; j++) {
-              if (this.cards[j].id == response.data[i].id) {
-                found = true;
-              }
-            }
-            if (!found) {
-              break;
-            }
-          }
-          if (found != true) {
-
-
-            if (response.data[i].card_faces == null) {
-              //a single faced card
-              tcard.name = response.data[i].name;
-              tcard.id = response.data[i].id;
-              tcard.mana_cost = response.data[i].mana_cost;
-              tcard.type = response.data[i].type_line;
-              tcard.oracle_text = response.data[i].oracle_text.replaceAll(tcard.name, "[CARD NAME]");
-              if (response.data[i].power != null) {
-                tcard.oracle_text += "\n" + response.data[i].power + "/" + response.data[i].toughness;
-              }
-              tcard.card_links = [];
-              tcard.art_links = [];
-              if (response.data[i].image_uris != null) {
-                if (response.data[i].image_uris.png != null) {
-                  tcard.card_links.push(response.data[i].image_uris.png);
-                }
-                if (response.data[i].image_uris.art_crop != null) {
-                  tcard.art_links.push(response.data[i].image_uris.art_crop);
-                }
-              }
-            } else {
-              //a double (or more) faced card
-              tcard.id = response.data[i].id;
-              tcard.name = response.data[i].card_faces[0].name;
-              tcard.mana_cost = response.data[i].card_faces[0].mana_cost;
-              tcard.type = response.data[i].card_faces[0].type_line;
-              tcard.oracle_text = response.data[i].card_faces[0].oracle_text.replaceAll(response.data[i].card_faces[0].name, "[CARD NAME]");
-              if (response.data[i].card_faces[0].power != null) {
-                tcard.oracle_text += "\n" + response.data[i].card_faces[0].power + "/" + response.data[i].card_faces[0].toughness;
-              }
-              tcard.card_links = [];
-              tcard.art_links = [];
-              if (response.data[i].card_faces[0].image_uris != null) {
-                if (response.data[i].card_faces[0].image_uris.png != null) {
-                  tcard.card_links.push(response.data[i].card_faces[0].image_uris.png);
-                }
-                if (response.data[i].card_faces[0].image_uris.art_crop != null) {
-                  tcard.art_links.push(response.data[i].card_faces[0].image_uris.art_crop);
-                }
-              }
-              for (var j = 1; j < response.data[i].card_faces.length; j++) {
-                tcard.name += "  //  " + response.data[i].card_faces[j].name;
-                tcard.mana_cost += "  //  " + response.data[i].card_faces[j].mana_cost;
-                tcard.type += "  //  " + response.data[i].card_faces[j].type_line;
-                tcard.oracle_text += "  //  " + response.data[i].card_faces[j].oracle_text.replaceAll(response.data[i].card_faces[j].name, "[CARD NAME]");
-                if (response.data[i].card_faces[j].power != null) {
-                  tcard.oracle_text += "\n" + response.data[i].card_faces[j].power + "/" + response.data[i].card_faces[j].toughness;
-                }
-                tcard.card_links = [];
-                tcard.art_links = [];
-                if (response.data[i].card_faces[j].image_uris != null) {
-                  if (response.data[i].card_faces[j].image_uris.png != null) {
-                    tcard.card_links.push(response.data[i].card_faces[j].image_uris.png);
-                  }
-                  if (response.data[i].card_faces[j].image_uris.art_crop != null) {
-                    tcard.art_links.push(response.data[i].card_faces[j].image_uris.art_crop);
-                  }
-                }
-              }
-            }
-            this.cards.push(tcard);
-            this.question_order.push(this.cards.length - 1);
-            this.refreshQuestions();
-            this.saveData();
-            this.add_count--;
-            if (this.add_count > 0) {
-              this.add_card();
-            }
-
-          } else {
-            this.scryfall_page++;
-            this.add_card();
-          }
-        }
-      }
-
-      Http.send();
+      this.add_count = 1;
+      this.scryfall_page = 1;
+      this.scryfall_get_func();
+    },
+    add_n_cards: function() {
+      this.scryfall_page = 1;
+      this.scryfall_get_func();
     },
     seed_cards: function() {
-      const Http = new XMLHttpRequest();
-      var query = "";
-      if (this.seed_string == "") {
-        query = "lang:english";
-      } else {
-        query = this.seed_string + " lang:english";
-      }
-      const url = 'https://api.scryfall.com/cards/search?order=' + this.seed_sort + '&unique=' + this.seed_unique + '&dir=' + this.seed_direction + '&include_extras=false&page=' + 1 + '&q=' + encodeURI(query);
-      Http.open("GET", url);
-
-      Http.onreadystatechange = (e) => {
-
-
-        if (Http.readyState == 4 && Http.status == 200) {
-
-          var response = JSON.parse(Http.responseText);
-          //console.log(response);
-          this.cards = [];
-          this.question_order = [];
-          var t_seed_num = app.seed_number < response.data.length ? app.seed_number : response.data.length;
-          for (var i = 0; i < t_seed_num; i++) {
-            var tcard = {};
-            if (response.data[i].card_faces == null) {
-              //a single faced card
-              tcard.name = response.data[i].name;
-              tcard.id = response.data[i].id;
-              tcard.mana_cost = response.data[i].mana_cost;
-              tcard.type = response.data[i].type_line;
-              tcard.oracle_text = response.data[i].oracle_text.replaceAll(tcard.name, "[CARD NAME]");
-              if (response.data[i].power != null) {
-                tcard.oracle_text += "\n" + response.data[i].power + "/" + response.data[i].toughness;
-              }
-              tcard.card_links = [];
-              tcard.art_links = [];
-              if (response.data[i].image_uris != null) {
-                if (response.data[i].image_uris.png != null) {
-                  tcard.card_links.push(response.data[i].image_uris.png);
-                }
-                if (response.data[i].image_uris.art_crop != null) {
-                  tcard.art_links.push(response.data[i].image_uris.art_crop);
-                }
-              }
-            } else {
-              //a double (or more) faced card
-              tcard.id = response.data[i].id;
-              tcard.name = response.data[i].card_faces[0].name;
-              tcard.mana_cost = response.data[i].card_faces[0].mana_cost;
-              tcard.type = response.data[i].card_faces[0].type_line;
-              tcard.oracle_text = response.data[i].card_faces[0].oracle_text.replaceAll(response.data[i].card_faces[0].name, "[CARD NAME]");
-              if (response.data[i].card_faces[0].power != null) {
-                tcard.oracle_text += "\n" + response.data[i].card_faces[0].power + "/" + response.data[i].card_faces[0].toughness;
-              }
-              tcard.card_links = [];
-              tcard.art_links = [];
-              if (response.data[i].card_faces[0].image_uris != null) {
-                if (response.data[i].card_faces[0].image_uris.png != null) {
-                  tcard.card_links.push(response.data[i].card_faces[0].image_uris.png);
-                }
-                if (response.data[i].card_faces[0].image_uris.art_crop != null) {
-                  tcard.art_links.push(response.data[i].card_faces[0].image_uris.art_crop);
-                }
-              }
-              for (var j = 1; j < response.data[i].card_faces.length; j++) {
-                tcard.name += "  //  " + response.data[i].card_faces[j].name;
-                tcard.mana_cost += "  //  " + response.data[i].card_faces[j].mana_cost;
-                tcard.type += "  //  " + response.data[i].card_faces[j].type_line;
-                tcard.oracle_text += "  //  " + response.data[i].card_faces[j].oracle_text.replaceAll(response.data[i].card_faces[j].name, "[CARD NAME]");
-                if (response.data[i].card_faces[j].power != null) {
-                  tcard.oracle_text += "\n" + response.data[i].card_faces[j].power + "/" + response.data[i].card_faces[j].toughness;
-                }
-                tcard.card_links = [];
-                tcard.art_links = [];
-                if (response.data[i].card_faces[j].image_uris != null) {
-                  if (response.data[i].card_faces[j].image_uris.png != null) {
-                    tcard.card_links.push(response.data[i].card_faces[j].image_uris.png);
-                  }
-                  if (response.data[i].card_faces[j].image_uris.art_crop != null) {
-                    tcard.art_links.push(response.data[i].card_faces[j].image_uris.art_crop);
-                  }
-                }
-              }
-            }
-            this.cards.push(tcard);
-            this.question_order.push(this.cards.length - 1);
-            //console.log(tcard);
-          }
-          this.refreshQuestions();
-          this.saveData();
-        }
-      }
-
-      Http.send();
-
+      this.cards = [];
+      this.question_order = [];
+      this.add_count = this.seed_number;
+      this.scryfall_page = 1;
+      this.scryfall_get_func();
     },
     calculateDifference: function(str1, str2) {
 
